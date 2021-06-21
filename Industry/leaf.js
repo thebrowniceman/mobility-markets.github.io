7b33e607892466f84b12f89d86ac70c5b7513ff0
thebrowniceman <84132991+thebrowniceman@users.noreply.github.com>
2021-06-21 14:30:57 +0200

    Update leaf.js

--- Industry/leaf.js
+++ Industry/leaf.js
@@ -3,11 +3,7 @@ var map = L.map( 'map', {
     minZoom: 2,
     zoom: 2
 });
-
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
 }).addTo( map );
-
-
-L.geoJson(countriesData, {style: style}).addTo(map);
