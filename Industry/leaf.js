2fb6f5b83d47426b946fb04e3da87336d503d043
thebrowniceman <84132991+thebrowniceman@users.noreply.github.com>
2021-06-21 20:26:14 +0200

    Update leaf.js

--- Industry/leaf.js
+++ Industry/leaf.js
@@ -100,7 +100,7 @@ var legend = L.control({position: 'bottomright'});
 legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 1, 2, 3, 5, 6, 7, 8],
+        grades = [0, 1, 2, 3, 4, 5, 6, 9],
         labels = [];
 
     // loop through our density intervals and generate a label with a colored square for each interval
