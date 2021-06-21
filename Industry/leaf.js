4df9d4f110f3667e0addf183843568399e07b342
thebrowniceman <84132991+thebrowniceman@users.noreply.github.com>
2021-06-21 13:39:38 +0200

    Update leaf.js

--- Industry/leaf.js
+++ Industry/leaf.js
@@ -1,7 +1,7 @@
 var map = L.map( 'map', {
     center: [53.0, 9.0],
     minZoom: 2,
-    zoom: 5
+    zoom: 2
 });
 L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
