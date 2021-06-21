14c7cd3e3e37bd6131c857baad5d4a92d426cfbc
thebrowniceman <84132991+thebrowniceman@users.noreply.github.com>
2021-06-21 14:28:34 +0200

    Update leaf.js

--- Industry/leaf.js
+++ Industry/leaf.js
@@ -9,26 +9,5 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     subdomains: ['a','b','c']
 }).addTo( map );
 
-function getColor(d) {
-    return d > 1000 ? '#b10026' :
-           d > 500  ? '#e31a1c' :
-           d > 200  ? '#fc4e2a' :
-           d > 100  ? '#fd8d3c' :
-           d > 50   ? '#feb24c' :
-           d > 20   ? '#fed976' :
-           d > 10   ? '#ffeda0' :
-		   d < 10   ? '#ffffcc' :
-                      '#FFFFFF';
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.measlesrate),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        fillOpacity: 0.7
-    };
-}
 
 L.geoJson(countriesData, {style: style}).addTo(map);
